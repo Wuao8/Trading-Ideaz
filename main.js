90d802bd5c34e73c05876faa7707a517de195572
Vanya <126008208+Wuao8@users.noreply.github.com>
2024-10-03 16:43:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-// Se hai bisogno di aggiornare dinamicamente i dati degli asset
-// puoi usare JavaScript per fare ciò.
-// Ad esempio, si possono aggiornare i nomi e i link tramite codice come segue:
+// Aggiorna i nomi e i link per ogni asset
+document.getElementById('asset-name-1').textContent = 'Nome Asset 1';
+document.getElementById('asset-link-1').href = 'LINK_1';
 
-document.getElementById('asset-name-1').textContent = Buy:'Btc-Usdt' target: 70.000usdt stop: 68000;
-document.getElementById('asset-link-1').href = 'https://it.tradingview.com/chart/AUl3TqhC/?symbol=BINANCE%3ABTCUSDT';
+document.getElementById('asset-name-2').textContent = 'Nome Asset 2';
+document.getElementById('asset-link-2').href = 'LINK_2';
+
+document.getElementById('asset-name-3').textContent = 'Nome Asset 3';
+document.getElementById('asset-link-3').href = 'LINK_3';
+
+document.getElementById('asset-name-4').textContent = 'Nome Asset 4';
+document.getElementById('asset-link-4').href = 'LINK_4';
+
+document.getElementById('asset-name-5').textContent = 'Nome Asset 5';
+document.getElementById('asset-link-5').href = 'LINK_5';
+
+document.getElementById('asset-name-6').textContent = 'Nome Asset 6';
+document.getElementById('asset-link-6').href = 'LINK_6';
+
+document.getElementById('asset-name-7').textContent = 'Nome Asset 7';
+document.getElementById('asset-link-7').href = 'LINK_7';
+
