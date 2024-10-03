45e5420a0c51702e2d98529107d7d5542e3f023e
Vanya <126008208+Wuao8@users.noreply.github.com>
2024-10-03 16:41:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@
 // puoi usare JavaScript per fare ciò.
 // Ad esempio, si possono aggiornare i nomi e i link tramite codice come segue:
 
-document.getElementById('asset-name-1').textContent = 'Nuovo Nome Asset 1';
-document.getElementById('asset-link-1').href = 'https://www.esempio.com/grafico1';
+document.getElementById('asset-name-1').textContent = Buy:'Btc-Usdt' target: 70.000usdt stop: 68000;
+document.getElementById('asset-link-1').href = 'https://it.tradingview.com/chart/AUl3TqhC/?symbol=BINANCE%3ABTCUSDT';
